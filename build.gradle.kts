5fe76f45fd177f2bf19edab7160e7f18faaa7efc
kartikdhiman <59189590+kartikdhiman@users.noreply.github.com>
2025-09-25 22:14:31 +0530

    feat: update application configuration to setup config client and upgrade Java version to 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
@@ -17,13 +17,22 @@ repositories {
     mavenCentral()
 }
 
+extra["springCloudVersion"] = "2025.1.0-M2"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<Test> {
